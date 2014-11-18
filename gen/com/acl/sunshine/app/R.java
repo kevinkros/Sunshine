4dc9410d95a98dd873bdf6f982f5be95f7e338e6
Kevin Kros <kevinkros@hotmail.com>
2014-11-18 00:00:11 -0500

    Added refresh and AsyncTask functionality

--- gen/com/acl/sunshine/app/R.java
+++ gen/com/acl/sunshine/app/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_refresh=0x7f080003;
+        public static final int action_settings=0x7f080004;
         public static final int container=0x7f080000;
         public static final int list_item_forecast_textview=0x7f080002;
         public static final int listview_forecast=0x7f080001;
@@ -36,9 +37,11 @@ public final class R {
         public static final int list_item_forecast=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int forecastfragment=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int action_refresh=0x7f050003;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
